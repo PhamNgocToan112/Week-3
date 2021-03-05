25ae96ad00d559df88579b5f03668fa1bf654561
PhamNgocToan112 <78184915+PhamNgocToan112@users.noreply.github.com>
2021-03-05 08:37:20 +0700

    Add files via upload

---
+++ A_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int arr1[10];
+int arr2[] = {1,2,3};
+int main()
+{
+	int n;
+	
+	n = 2;
+	int arr3[n] = {1, 2, 3, 4};
+	
+	n = 6;
+	int arr4[n] = {1, 2, 3, 4};
+	
+	arr5[] = {1, 2, 3, 4};
+	return 0;
+}
